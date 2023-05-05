878d59c471756722cb80f9562e99b76f4b3fd90f
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-05-05 20:18:44 +0400

    add settings for reset password

--- backend/src/users/api/v1/serializers.py
+++ backend/src/users/api/v1/serializers.py
@@ -9,32 +9,38 @@ from djoser.serializers import UserCreateMixin
 class CreateUserSerializer(UserCreateMixin, serializers.ModelSerializer):
     """Сериализация при регистрации пользователя"""
 
-    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
-    password_confirm = serializers.CharField(style={"input_type": "password"}, write_only=True)
+    password = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
+    password_confirm = serializers.CharField(
+        style={"input_type": "password"}, write_only=True
+    )
 
     class Meta:
         model = User
         fields = ['email', 'username', 'password', 'password_confirm']
-    
+
     def validate(self, attrs: dict):
         password = attrs.get("password")
         password_confirm = attrs.get("password_confirm")
         attrs.pop('password_confirm')
         user = User(**attrs)
-        print(password_confirm)
         if password == password_confirm:
             try:
                 validate_password(password, user)
             except django_exceptions.ValidationError as e:
                 serializer_error = serializers.as_serializer_error(e)
                 raise serializers.ValidationError(
-                    {"password": serializer_error[api_settings.NON_FIELD_ERRORS_KEY]}
+                    {"password": serializer_error[
+                        api_settings.NON_FIELD_ERRORS_KEY
+                    ]
+                    }
                 )
 
             return attrs
         raise serializers.ValidationError(
-                    {
-                        "password": 'The passwords entered do not match',
-                        "password_confirm": 'The passwords entered do not match'
-                    }
-                )
+            {
+                "password": 'The passwords entered do not match',
+                "password_confirm": 'The passwords entered do not match'
+            }
+        )
--- backend/src/users/api/v1/urls.py
+++ backend/src/users/api/v1/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path, include, re_path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
-from src.users.api.v1.views import CreateUserView
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView
+# )
 
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    # path('create/', CreateUserView.as_view()),
+    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
 ]
