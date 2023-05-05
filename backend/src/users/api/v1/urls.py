f37ea151fff10576e4a4ccb3fbe6672f7042243c
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-05-05 22:30:25 +0400

    add custum function for reset password email

--- backend/src/users/api/v1/urls.py
+++ backend/src/users/api/v1/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include, re_path
+from src.users.api.v1.views import CustomUserViewSet
+from django.contrib.auth import get_user_model
+from rest_framework.routers import DefaultRouter
 # from rest_framework_simplejwt.views import (
 #     TokenObtainPairView,
 #     TokenRefreshView,
@@ -6,10 +9,18 @@ from django.urls import path, include, re_path
 # )
 
 
+router = DefaultRouter()
+router.register("users", CustomUserViewSet)
+
+User = get_user_model()
+
+
 urlpatterns = [
     # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.jwt')),
+    re_path(r'^auth/', include('djoser.urls.jwt'))
 ]
+
+urlpatterns += router.urls
--- backend/src/users/api/v1/views.py
+++ backend/src/users/api/v1/views.py
@@ -4,8 +4,42 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from src.users.api.v1.serializers import CreateUserSerializer
+from djoser.views import UserViewSet
+from djoser.email import PasswordResetEmail
+from django.contrib.auth.models import User
+from djoser.compat import get_user_email
+from djoser.conf import settings
+from rest_framework.decorators import action
+from rest_framework.routers import DefaultRouter
 
 
-class CreateUserView(generics.CreateAPIView):
-    """Создание нового пользователя"""
-    serializer_class = CreateUserSerializer
+def send_reset_password_email(context, email):
+    try:
+        context['user'] = User.objects.get(id=context.get('user_id'))
+        PasswordResetEmail(context=context).send(email)
+    except Exception as exc:
+        print('Ошибка')
+        # raise self.retry(exc=exc, countdown=60)
+
+
+class CustomUserViewSet(UserViewSet):
+    # serializer_class = CreateUserSerializer
+
+    @action(["post"], detail=False)
+    def reset_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user()
+        print('Это моя функция')
+        if user:
+            send_reset_password_email(
+                {
+                    'user_id': user.id,
+                    'domain': request.get_host(),
+                    'protocol': 'https' if request.is_secure() else 'http',
+                    'site_name': request.get_host()
+                },
+                [get_user_email(user)]
+            )
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
