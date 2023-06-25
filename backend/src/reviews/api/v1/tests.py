ed08167acab831590012754000c91b50b6358bb0
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-06-26 01:43:36 +0400

    add new create review tests

--- backend/src/reviews/api/v1/tests.py
+++ backend/src/reviews/api/v1/tests.py
@@ -51,6 +51,7 @@ class ReviewTestCase(CreateUsersTestCase):
             self.assertEqual(new_review.get('text'), text)
             self.users[key].update({'text': text})
 
+        # проверяем, что отзывы добавились
         self.published_reviews()
         response = self.client.get(path=reverse('reviews'))
         self.assertIn('data', response.json().keys())
@@ -120,6 +121,22 @@ class ReviewTestCase(CreateUsersTestCase):
             self.users[key].update({'last_name': last_name})
             self.users[key].update({'phone_number': phone_number})
 
+            # проверяем что данные поменялись
+            response = self.client.get(
+                path=reverse('user'),
+                headers=auth_data
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('data', response.json().keys())
+            user_data = response.json().get('data')
+            self.assertEqual(
+                user_data.get('first_name'), user.get('first_name')
+            )
+            self.assertEqual(user_data.get('last_name'), user.get('last_name'))
+            self.assertEqual(
+                user_data.get('phone_number'), user.get('phone_number')
+            )
+
         # проверяем, что отзывы добавились
         self.published_reviews()
         response = self.client.get(path=reverse('reviews'))
@@ -140,20 +157,6 @@ class ReviewTestCase(CreateUsersTestCase):
             self.assertContains(
                 response, user.get('text'), count=1
             )
-
-        # проверяем что данные поменялись
-        response = self.client.get(
-            path=reverse('user'),
-            headers=auth_data
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('data', response.json().keys())
-        user_data = response.json().get('data')
-        self.assertEqual(user_data.get('first_name'), user.get('first_name'))
-        self.assertEqual(user_data.get('last_name'), user.get('last_name'))
-        self.assertEqual(
-            user_data.get('phone_number'), user.get('phone_number')
-        )
 
     def test_create_review_by_anonimus_user(self):
         """
@@ -265,3 +268,123 @@ class ReviewTestCase(CreateUsersTestCase):
         all_reviews = response.json().get('data')
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(all_reviews.get('count'), len(users))
+
+    def test_create_review_by_user_without_phone_number_without_add_data(self):
+        """
+        Проверяет добавление отзыва пользователем,
+        у которого нет номера телефона для добавления отзыва
+        без его передачи для отзыва
+        """
+        self.create_data_for_review()
+        users = self.users
+        for _, user in users.items():
+            token = user.get('token')
+            text = fake.text(max_nb_chars=60)
+            auth_data = {
+                'Authorization': f'Bearer {token}'
+            }
+            text = fake.text(max_nb_chars=60)
+            review_data = {
+                'text': text,
+                'rating': 5
+            }
+
+            response = self.client.post(
+                path=reverse('review-add'),
+                data=review_data,
+                headers=auth_data
+            )
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('errors', response.json().keys())
+            errors = response.json().get('errors')
+            self.assertEqual(
+                errors.get('phone_number')[0],
+                'This field may not be blank.'
+            )
+
+        # проверяем что отзывов нет
+        self.published_reviews()
+        response = self.client.get(path=reverse('reviews'))
+        self.assertIn('data', response.json().keys())
+        all_reviews = response.json().get('data')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(all_reviews.get('count'), len(users))
+
+    def test_create_review_by_user_without_phone_number_with_add_data(self):
+        """
+        Проверяет добавление отзыва пользователем,
+        у которого нет номера телефона для добавления отзыва
+        и передаем его для добавление отзыва
+        """
+        self.create_data_for_review()
+        users = self.users
+        for key, user in users.items():
+            token = user.get('token')
+            text = fake.text(max_nb_chars=60)
+            auth_data = {
+                'Authorization': f'Bearer {token}'
+            }
+            text = fake.text(max_nb_chars=60)
+            phone_number = '8' + fake.msisdn()
+            review_data = {
+                'text': text,
+                'rating': 5,
+                'phone_number': phone_number
+            }
+
+            response = self.client.post(
+                path=reverse('review-add'),
+                data=review_data,
+                headers=auth_data
+            )
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('data', response.json().keys())
+            new_review = response.json().get('data')
+            self.assertEqual(
+                new_review.get('first_name'), user.get('first_name'))
+            self.assertEqual(
+                new_review.get('last_name'), user.get('last_name'))
+            self.assertEqual(
+                new_review.get('phone_number'), phone_number)
+            self.assertEqual(
+                new_review.get('rating'), 5)
+            self.assertEqual(new_review.get('text'), text)
+            self.users[key].update({'text': text})
+            self.users[key].update({'phone_number': phone_number})
+
+            # проверяем что данные поменялись
+            response = self.client.get(
+                path=reverse('user'),
+                headers=auth_data
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('data', response.json().keys())
+            user_data = response.json().get('data')
+            self.assertEqual(
+                user_data.get('first_name'), user.get('first_name')
+            )
+            self.assertEqual(user_data.get('last_name'), user.get('last_name'))
+            self.assertEqual(
+                user_data.get('phone_number'), user.get('phone_number')
+            )
+
+        # проверяем что отзывы добавились
+        self.published_reviews()
+        response = self.client.get(path=reverse('reviews'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('data', response.json().keys())
+        all_reviews = response.json().get('data')
+        self.assertEqual(all_reviews.get('count'), len(users))
+        for user in users.values():
+            self.assertContains(
+                response, user.get('first_name'), count=1
+            )
+            self.assertContains(
+                response, user.get('last_name'), count=1
+            )
+            self.assertContains(
+                response, user.get('phone_number'), count=1
+            )
+            self.assertContains(
+                response, user.get('text'), count=1
+            )
